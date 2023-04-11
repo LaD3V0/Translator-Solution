0d55bd08e8bce2f2f4cf8b00b850462fc73d3e0a
DEVIN CARTER <DCarter7101@slps.org>
2023-04-11 10:50:41 -0500

    Fixed a minor error

--- Translator Project/Form1.vb
+++ Translator Project/Form1.vb
@@ -4,26 +4,26 @@
     End Sub
     Private Function ItalianLang() As String
         Dim ItalianText As String = String.Empty
-        If radBrother.Checked And (cboLang.SelectedIndex = 1) = True Then
+        If radBrother.Checked And (cboLang.SelectedIndex = 2) = True Then
             ItalianText = "Fratello"
-        ElseIf radFather.Checked And (cboLang.SelectedIndex = 1) = True Then
+        ElseIf radFather.Checked And (cboLang.SelectedIndex = 2) = True Then
             ItalianText = "Padre"
-        ElseIf RadMother.Checked And (cboLang.SelectedIndex = 1) = True Then
+        ElseIf RadMother.Checked And (cboLang.SelectedIndex = 2) = True Then
             ItalianText = "Madre"
-        ElseIf radSister.Checked And (cboLang.SelectedIndex = 1) = True Then
+        ElseIf radSister.Checked And (cboLang.SelectedIndex = 2) = True Then
             ItalianText = "Sorella"
         End If
         Return ItalianText
     End Function
     Private Function SpanishLang() As String
         Dim SpanishText As String = String.Empty
-        If radBrother.Checked And (cboLang.SelectedIndex = 2) = True Then
+        If radBrother.Checked And (cboLang.SelectedIndex = 1) = True Then
             SpanishText = "Hermano"
-        ElseIf radFather.Checked And (cboLang.SelectedIndex = 2) = True Then
+        ElseIf radFather.Checked And (cboLang.SelectedIndex = 1) = True Then
             SpanishText = "Padre"
-        ElseIf RadMother.Checked And (cboLang.SelectedIndex = 2) = True Then
+        ElseIf RadMother.Checked And (cboLang.SelectedIndex = 1) = True Then
             SpanishText = "Madre"
-        ElseIf radSister.Checked And (cboLang.SelectedIndex = 2) = True Then
+        ElseIf radSister.Checked And (cboLang.SelectedIndex = 1) = True Then
             SpanishText = "Hermana"
         End If
         Return SpanishText
@@ -48,15 +48,13 @@
 
     Private Sub btnTranslate_Click(sender As Object, e As EventArgs) Handles btnTranslate.Click
 
-        If cboLang.Text = "Spanish" Then
-            lblTranslated.Text = SpanishLang()
-        ElseIf cboLang.Text = "French" Then
+        If cboLang.Text = "French" Then
             lblTranslated.Text = FrenchLang()
+        ElseIf cboLang.Text = "Spanish" Then
+            lblTranslated.Text = SpanishLang()
         ElseIf cboLang.Text = "Italian" Then
             lblTranslated.Text = ItalianLang()
         End If
-        'ivdiyaisrvbcoiuawbo
-
     End Sub
     Private Sub ClearOuput(sender As Object, e As EventArgs) Handles radBrother.CheckedChanged,
     radFather.CheckedChanged, radSister.CheckedChanged, RadMother.CheckedChanged, cboLang.SelectedIndexChanged
